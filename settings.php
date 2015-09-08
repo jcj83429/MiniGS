0dfb0beeb762c568083011abf3c055606d59ed74
jcj83429 <jcj83429@gmail.com>
2015-09-08 01:59:28 -0700

    ignore settings.php (avoid accidentally commiting my account/pw)
    also added hints in settings.php

--- settings.php
+++ settings.php
@@ -2,6 +2,6 @@
 const MYSQL_ACCOUNT = '';
 const MYSQL_PASSWORD = '';
 const DB_NAME = 'minigrooveshark';
-const MUSIC_FOLDER = '/home/user/Music/';
-const COMPRESSED_CACHE = '/tmp/php-music-compress-out/';
+const MUSIC_FOLDER = '/home/user/Music/'; //must end with slash
+const COMPRESSED_CACHE = '/tmp/php-music-compress-out/'; //must end with slash
 ?>
